7f57355ef306f3e5a62c3008b0c60e97d8b199e1
anupkrbid <anupbid94@gmail.com>
2018-07-29 17:33:38 +0530

    Toggle Sidebar in Mobile View

--- src/App/components/Layout/Toolbar/Toolbar.jsx
+++ src/App/components/Layout/Toolbar/Toolbar.jsx
@@ -6,8 +6,8 @@ import Navigations from '../Navigations/Navigations';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <button className={classes.Menu} onClick={props.openSideDrawer}>
-      <i class="fa fa-3x fa-bars" />
+    <button className={classes.MobileOnly} onClick={props.openSideDrawer}>
+      <i className="fa fa-3x fa-bars" />
     </button>
     <div className={classes.Logo}>
       <Logo />
